77eb301d95e0b01efe45d6a568fe5af5355e5993
yash0305 <kesharwaniyash20@gmail.com>
2022-11-24 07:11:29 -0800

    added dueDate should be greater than timestamp

--- todo/app/views.py
+++ todo/app/views.py
@@ -1,12 +1,11 @@
-from rest_framework.viewsets import ModelViewSet
 from .models import Tag, Todo
 from .serializers import TagSerializers, TodoSerializers
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from datetime import datetime
 
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication, BasicAuthentication])
@@ -30,7 +29,14 @@ def taskDetail(request,pk):
 
 @api_view(['POST'])
 def taskCreate(request):
-    new = TodoSerializers(data=request.data)
+    data = request.data
+    t = data["Timestamp"]
+    timeStamp = datetime.strptime(t[:10], "%Y-%m-%d")
+    dueDate = datetime.strptime(data["Due_Date"], "%Y-%m-%d")
+    if dueDate > timeStamp:
+        new = TodoSerializers(data=data)
+    else: 
+        return Response("Due Date field value cannot be before Timestamp created")
 
     if new.is_valid():
         new.save()
